eb8d43c650bbf2635cf3b4e767930aca7fbe7307
yatabebe <c0b23143fb@edu.teu.ac.jp>
2024-11-19 15:30:23 +0900

    練習１：背景画像の描画

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600)) #surface
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #問題1 surface
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0]) #screen sarfaceに背景画像surfaceを貼り付ける
+        #(背景画像surface, 画面の位置)
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
