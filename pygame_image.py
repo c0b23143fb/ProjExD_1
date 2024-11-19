560022771c3fcbaecd24140692f88077c9f593d3
yatabebe <c0b23143fb@edu.teu.ac.jp>
2024-11-19 16:12:39 +0900

    練習７：ループを解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600)) #surface
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #問題1 背景画像surfaceを作成する
+    bg_img2 = pg.transform.flip(bg_img, True, False) #フリップ下背景surface
     flykk_img = pg.image.load("fig/3.png") 
     flykk_img = pg.transform.flip(flykk_img, True, False) #左右反転 #(画像surface, 左右を反転させるか、上下を反転させるか)
 
@@ -17,9 +18,12 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%800 #練習6-2
-        screen.blit(bg_img, [-x, 0]) #screen sarfaceに背景画像surfaceを貼り付ける
+        x = -(tmr%3200)
+        screen.blit(bg_img, [x, 0]) #screen sarfaceに背景画像surfaceを貼り付ける
         #(背景画像surface, 画面の位置) #問題3
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
         screen.blit(flykk_img, [300, 200]) #screen sarfaceにこうかとん画像surfaceを貼り付ける
         pg.display.update()
         tmr += 1        
